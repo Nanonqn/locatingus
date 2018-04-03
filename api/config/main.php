924a04a5a2dc56ec75561afc677ce9bd829d4e5c
Mariano <nano.programacion@gmail.com>
2018-04-02 23:30:42 -0300

    tablas 'Country' y 'City'

--- api/config/main.php
+++ api/config/main.php
@@ -41,7 +41,7 @@ return [
         ],
         
         'urlManager' => [
-            'enablePrettyUrl' => true,
+            'enablePrettyUrl' => false,
             'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
